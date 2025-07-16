74e1405278afae476e86ee3f57d9e275ec88522c
Princy chahal <princychahal68@gmail.com>
2025-07-16 08:26:39 -0700

    initial commit

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,20 @@
+import EpisodeDetails from "../episodes/EpisodeDetails";
+import EpisodeList from "../episodes/EpisodeList";
 import "./shows.css";
+import {useState} from "react";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
-  return <div className="show-details"></div>;
+export default function ShowDetails({show}) {
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  if (!show) {
+    return <p>Please selecte a show</p>
+  }
+
+  return <div className="show-details">
+    <EpisodeList name={show.name} episodes = {show.episodes} selectedEpisode={selectedEpisode} setSelectedEpisode={setSelectedEpisode}>
+
+    </EpisodeList>
+    <EpisodeDetails episode={selectedEpisode}></EpisodeDetails>
+  </div>;
 }
